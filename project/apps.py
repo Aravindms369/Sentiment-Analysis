b35587689b12c7d2b4697f93441acb0cb5f9660e
Aravindms369 <55455165+Aravindms369@users.noreply.github.com>
2020-07-29 19:39:16 +0530

    final commits

---
+++ project/apps.py
@@ -0,0 +1,62 @@
+﻿import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+
+st.title("Sentiment analysis of tweets about US airlines")
+st.sidebar.title("Sentiment analysis of tweets about US airlines")
+st.markdown("This application is Streamlit dashboard to analyse sentiment of tweets ð�?¦")
+st.sidebar.markdown("This application is Streamlit dashboard to analyse sentiment of tweets ð�?¦")
+DATA_URL = ("/prjects/Sentiment-Analysis/project/Tweets.csv")
+
+@st.cache(persist=True)
+def load_data():
+    data = pd.read_csv(DATA_URL)
+    data['tweet_created'] = pd.to_datetime(data['tweet_created'])
+    return data
+
+data = load_data()
+st.sidebar.subheader("Show random tweet")
+random_tweet = st.sidebar.radio('Sentiment', ('positive', 'neutral', 'negative'))
+st.sidebar.markdown(data.query('airline_sentiment == @random_tweet')[["text"]].sample(n=1).iat[0,0])
+
+st.sidebar.markdown("###Number of tweets by sentiment")
+select = st.sidebar.selectbox('visualization type', ['Histogram', 'Pie Chart'], key='1')
+sentiment_count = data['airline_sentiment'].value_counts()
+sentiment_count = pd.DataFrame({'Sentiment':sentiment_count.index, 'Tweets':sentiment_count.values})
+
+
+if not st.sidebar.checkbox("Hide", True):
+    st.markdown("###Number of tweets by sentiment")
+    if select == "Histogram":
+        fig = px.bar(sentiment_count, x='Sentiment', y='Tweets', height=500)
+        st.plotly_chart(fig)
+    else:
+        fig = px.pie(sentiment_count, values='Tweets', names='Sentiment')
+        st.plotly_chart(fig)
+
+
+
+st.sidebar.subheader("Breakdown airline tweets by sentiment")
+choice = st.sidebar.multiselect('Pick Airlines', ('US Airways','United','American','Southwest','Delta','Virgin America'), key='0')
+
+if len(choice)>0:
+    choice_data = data[data.airline.isin(choice)]
+    fig_choice = px.histogram(choice_data, x='airline', y='airline_sentiment', histfunc='count', color='airline_sentiment', facet_col='airline_sentiment', labels={'airline_sentiment':'tweets'}, height=600, width=800)
+    st.plotly_chart(fig_choice)
+
+st.sidebar.header("Word Cloud")
+word_sentiment = st.sidebar.radio('Display Word Cloud for what sentiment?', ('positive', 'neutral', 'negative'))
+
+if not st.sidebar.checkbox("Close", True, key='3'):
+    st.header('Word Cloud for %s sentiment' % (word_sentiment))
+    df = data[data['airline_sentiment']==word_sentiment]
+    words = ' '.join(df['text'])
+    processed_words = ' '.join([word for word in words.split() if 'http' not in word and not word.startswith('@') and word!= 'RT' ])
+    wordcloud = WordCloud(stopwords=STOPWORDS, background_color='white', height=640, width=800).generate(processed_words)
+    plt.imshow(wordcloud)
+    plt.xticks([])
+    plt.yticks([])
+    st.pyplot()
